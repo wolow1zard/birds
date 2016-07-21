87e2ba6fc05e666e3e4f72d7bd1ba0b1a2834e08
Mikael Tönnberg <mikael.tonnberg@antura.se>
2016-07-22 01:30:49 +0200

    added the missing file

---
+++ BirdApi/BirdApi.Tests/Models/BirdDtoTests.cs
@@ -0,0 +1,19 @@
+﻿using BirdsApi.Models;
+using NUnit.Framework;
+
+namespace BirdApi.Tests.Business
+{
+    [TestFixture]
+    public class BirdDtoTests
+    {
+        [Test]
+        public void Should_BeVisibleByDefault()
+        {
+            // Arrange
+            var model = new BirdDto();
+
+            // Assert
+            Assert.AreEqual(false, model.Visible);
+        }
+    }
+}
