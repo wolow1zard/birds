a47fbc486f4601dcb0037bbf5dee36db67c85781
Mikael Tönnberg <mikael.tonnberg@antura.se>
2016-07-21 15:53:15 +0200

    added a skeleton for the CRUD api

---
+++ src/BirdsApi/Controllers/BirdsV1Controller.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BirdsApi.Controllers
+{
+    [Route("v1")]
+    public class BirdsV1Controller : Controller
+    {
+        public BirdsV1Controller()
+        {
+        }
+
+        /// <summary>
+        /// Gets a testmodel with the given id, if no match is found an error is thrown
+        /// </summary>
+        [HttpGet("birds")]
+        public string Get()
+        {
+            return "birds";
+        }
+
+        [HttpPost("birds")]
+        public void Post(string webhookId)
+        {
+            // TODO implement
+        }
+
+        [HttpGet("birds/{birdId}")]
+        public string Get(string birdId)
+        {
+             return "a bird";
+             // TODO implement
+        }
+
+        [HttpPut("birds/{birdId}")]
+        public void Put(int birdId)
+        {
+             // TODO implement
+        }
+
+        [HttpDelete("birds/{birdId}")]
+        public void Delete(string birdId)
+        {
+            // TODO implement
+        }
+    }
+}
---
+++ src/BirdsApi/Controllers/SwaggerRedirectController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BirdsApi.Controllers
+{
+    [Route("")]
+    [ApiExplorerSettings(IgnoreApi = true)]
+    public class SwaggerRedirectController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public RedirectResult Index()
+        {
+            return new RedirectResult("/swagger/ui/index.html");
+        }
+    }
+}
